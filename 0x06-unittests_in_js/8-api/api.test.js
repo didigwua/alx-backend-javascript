c16b3fad73d1695de28e8e7f603691f5f6e3730a
Abuchi John Didigwu <106770396+didigwua@users.noreply.github.com>
2023-06-15 21:05:50 +0100

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,28 @@
-const request = require('request');
-const chai = require('chai')
-const { expect } = require('chai');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('API test', () => {
-      it('respond with 200 code and body', (done) => {
-        request('http://localhost:7865', 'GET', (err, res, body) => {
-          if (err) throw err;
-          expect(res.statusCode).to.equal(200);
-          expect(body).to.equal('Welcome to the payment system');
-        });
-        done();
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
     });
 });
